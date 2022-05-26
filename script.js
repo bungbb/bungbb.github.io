69d448b8feeb11fedf829e4b0b624e68684d6df8
bungbb <89338938+bungbb@users.noreply.github.com>
2022-05-26 12:37:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ console.log(queryString);
 const urlParams = new URLSearchParams(queryString);
 const auth_key = urlParams.get("code")
 document.getElementById("code").innerHTML = auth_key;
+
+function copyButton() {
+  var copyText = document.getElementById(auth_key);
+  copyText.select();
+  navigator.clipboard.writeText(copyText.value);
+  alert("Copied the text: " + copyText.value);
+}
